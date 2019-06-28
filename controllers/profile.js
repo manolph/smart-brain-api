0c71234c7cd1fa67b9a2c91a5846c37f18595b9f
manolph <manol@webpoint.io>
2019-06-28 16:11:05 -0500

    first commit

---
+++ controllers/profile.js
@@ -0,0 +1,34 @@
+const handleProfileGet = (req, res, db) => {
+  const { id } = req.params;
+  db.select('*').from('users').where({id})
+    .then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('Not found')
+      }
+    })
+    .catch(err => res.status(400).json('error getting user'))
+}
+
+const handleProfileUpdate = (req, res, db) => {
+  const { id } = req.params;
+  const { name, age, pet } = req.body.formInput;
+  console.log(name, age, pet, id);
+  
+  db('users').where({ id })
+  .update({ name })
+  .then(resp => {
+    if(resp) {
+     return res.json("success")
+    } else {
+      return res.status(400).json('unable to update');
+    }
+  })
+  .catch(err => res.status(400).json("error updating user"));
+}
+
+module.exports = {
+  handleProfileGet,
+  handleProfileUpdate
+}
