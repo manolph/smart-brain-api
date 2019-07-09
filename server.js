1ed9e189603777830c79cad2593212e2ffc89a66
manolph <manol@webpoint.io>
2019-07-09 11:15:07 -0500

    changing port on server.js to environmental variable PORT

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, 
 app.post('/imageurl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3000, ()=> {
-  console.log(`app is running on port 3000 ${PORT}`);
+  console.log(`app is running on port ${process.env.PORT}`);
 })
